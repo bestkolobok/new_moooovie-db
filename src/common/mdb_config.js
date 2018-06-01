3c3012a83d44db5f8003294613d3d31805d19cb2
Alex <alex.guzey@gmail.com>
2018-06-01 20:05:21 +0300

    add master

--- src/common/mdb_config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "8e2cc7ff01fa51e26900ce7e019c1293",
-    language: "ru-RUS",
-    region: "UA",
-    releaseMonthBefore: 9,
-    releaseMonthAfter: -1,
-    nowPlaingMonthAfter: -3
-}
